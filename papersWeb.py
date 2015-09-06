e7969133e3723ff0276152d8bf5b884ddcee74b5
Andrew Cumming <andrew.cumming@gmail.com>
2015-09-06 15:09:56 -0400

    Added footer with statistics

--- papersWeb.py
+++ papersWeb.py
@@ -11,6 +11,7 @@ years=[]
 titles=[]
 dates=[]
 filenames=[]
+sizes=[]
 
 for filename in os.listdir(papersDirectory):
 	if filename[0] != '.':
@@ -21,10 +22,11 @@ for filename in os.listdir(papersDirectory):
 		title = data[2]
 		authors.append(author)
 		years.append(year)
-		titles.append(title.split('.')[0])
+		titles.append(title.split('.')[0])   # removes the .pdf from the filename
 		filenames.append(papersDirectory+filename)
 		date = datetime.datetime.fromtimestamp(os.path.getmtime(papersDirectory+filename))
 		dates.append(str(date))
+		sizes.append(os.path.getsize(papersDirectory+filename))
 
 print('''<HTML>
   <HEAD>
@@ -33,9 +35,9 @@ print('''<HTML>
 </head>
 <body>''')
 
-print('|<a href="http://adsabs.harvard.edu/abstract_service.html" target="_blank">ADS</a>')
-print('|<a href="file://'+papersDirectory+'.DS_Store">Finder</a>')
-print('|')
+print('| <a href="http://adsabs.harvard.edu/abstract_service.html" target="_blank">ADS</a> ')
+print('| <a href="file://'+papersDirectory+'.DS_Store">Finder</a> ')
+print('|')	
 		
 print('<table style="width:100\%" class="sortable"')
 print('<tr><th style="width:8em">Author</th><th style="width:4em">Year</th><th>Title</th><th>Date added</th></tr>')
@@ -50,9 +52,14 @@ for author,year,title,filename,date in zip(authors,years,titles,filenames,dates)
 	
 print('</table>')
 
+totalSize = sum(sizes)
+
 stop = time.time()
 #print('<br><hr>')
 #print('<p><small>%d papers in %g seconds</small></p>' % (len(authors),stop-start))
+
+print('<br><small>Stats: %d papers %.3g MB %.3g ms</small>' % (len(authors),totalSize/1e6,1000.0*(stop-start)))
+
 print('''
 </body>
 </html>''')
